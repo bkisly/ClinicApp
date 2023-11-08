67baa1582c8379d2c220590ca29e9380a8867cf2
bkisly <b.kisly@gmail.com>
2023-11-08 11:14:11 +0100

    Added local db to the project

--- ClinicApp/Program.cs
+++ ClinicApp/Program.cs
@@ -1,6 +1,12 @@
+using ClinicApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection")));
 
 var app = builder.Build();
 
