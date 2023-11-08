a73fa8a46a99db3c75256dd517a0a9798f32ac9f
bkisly <b.kisly@gmail.com>
2023-11-08 11:05:15 +0100

    Added sample controller

---
+++ ClinicApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("Hello world! (from controller)");
+        }
+    }
+}
