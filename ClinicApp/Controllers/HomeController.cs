c3d5a105956c877d085fbf386e14f43102497b1b
bkisly <b.kisly@gmail.com>
2023-11-29 09:46:34 +0100

    Customized home page

--- ClinicApp/Controllers/HomeController.cs
+++ ClinicApp/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 ﻿using ClinicApp.Infrastructure;
-using ClinicApp.Models.Dto;
-using ClinicApp.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClinicApp.Controllers
@@ -9,13 +7,13 @@ namespace ClinicApp.Controllers
     {
         public IActionResult Index()
         {
-            string? role = null;
+            string? customContent = null;
 
-            if(User.IsInRole(Constants.Roles.ManagerRoleName)) role = Constants.Roles.ManagerRoleName;
-            else if(User.IsInRole(Constants.Roles.DoctorRoleName)) role = Constants.Roles.DoctorRoleName;
-            else if(User.IsInRole(Constants.Roles.PatientRoleName)) role = Constants.Roles.PatientRoleName;
+            if (User.IsInRole(Constants.Roles.ManagerRoleName)) customContent = "Manager";
+            else if (User.IsInRole(Constants.Roles.DoctorRoleName)) customContent = "Doctor";
+            else if (User.IsInRole(Constants.Roles.PatientRoleName)) customContent = "Patient";
 
-            return View(nameof(Index), role);
+            return View(nameof(Index), customContent);
         }
     }
 }
