90dea310fd323e02fabc2d3375bf37e22e62debc
bkisly <b.kisly@gmail.com>
2024-01-13 21:14:22 +0100

    Created registration form

--- ClinicApp/ViewModels/RegistrationViewModel.cs
+++ ClinicApp/ViewModels/RegistrationViewModel.cs
@@ -1,11 +1,13 @@
-﻿namespace ClinicApp.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicApp.ViewModels
 {
     public class RegistrationViewModel
     {
-        public string UserName { get; set; } = string.Empty;
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public string ConfirmPassword { get; set; } = string.Empty;
+        [Required] public string UserName { get; set; } = string.Empty;
+        [Required] public string FirstName { get; set; } = string.Empty;
+        [Required] public string LastName { get; set; } = string.Empty;
+        [Required] public string Password { get; set; } = string.Empty;
+        [Required] public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
