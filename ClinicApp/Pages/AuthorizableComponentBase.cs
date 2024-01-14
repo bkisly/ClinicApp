636a6bf644577838866d458a644f9718c17f53e1
bkisly <b.kisly@gmail.com>
2024-01-14 21:12:29 +0100

    Created visits list component

---
+++ ClinicApp/Pages/AuthorizableComponentBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace ClinicApp.Pages
+{
+    public class AuthorizableComponentBase : ComponentBase
+    {
+        [CascadingParameter] protected Task<AuthenticationState>? AuthenticationState { get; set; }
+    }
+}
